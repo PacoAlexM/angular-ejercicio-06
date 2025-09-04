1570746dd887fcd9fae38a96ecc88a5e726ced8b
PacoAlexM <pako_ale_martell@hotmail.com>
2025-09-03 22:57:43 -0600

    Ejercicio de paises

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
     selector: 'country-page',
-    imports: [],
+    imports: [ReactiveFormsModule, JsonPipe],
     templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent {}
+export class CountryPageComponent {
+    formBuilder = inject(FormBuilder);
+    countryService = inject(CountryService);
+
+    regions = signal(this.countryService.regions);
+    countriesByRegion = signal<Country[]>([]);
+    borders = signal<Country[]>([]);
+
+    myForm = this.formBuilder.group({
+        region: ['', [Validators.required]],
+        country: ['', [Validators.required]],
+        border: ['', [Validators.required]],
+    });
+
+    onFormChanged = effect(onCleanup => {
+        const regionSubscription = this.onRegionChanged();
+
+        onCleanup(() => {
+            regionSubscription.unsubscribe();
+        })
+    });
+
+    onRegionChanged() {
+        return this.myForm.get('region')!.valueChanges
+            .pipe(
+                tap(() => this.myForm.get('country')!.setValue('')),
+                tap(() => this.myForm.get('border')!.setValue('')),
+                tap(() => {
+                    this.countriesByRegion.set([]);
+                    this.borders.set([]);
+                }),
+                switchMap(region => this.countryService.getCountriesByRegion(region ?? ''))
+            )
+            .subscribe(countries => {
+                this.countriesByRegion.set(countries);
+            });
+    }
+}
